ab8235d1ded3be4644cfa69a157d4e41ee8f39c2
PengGuangYao <guangyao.peng@raykite.com>
2021-12-20 14:57:10 +0800

    新增useInterval

--- src/hooks/use-interval.ts
+++ src/hooks/use-interval.ts
@@ -2,16 +2,14 @@ import {useEffect, useRef} from 'react';
 
 const useInterval = (fn:()=> void, delay:number = 0) => {
     const callRef = useRef(fn);
-
     useEffect(() => {
         callRef.current = fn;
     })
-
     useEffect(() => {
-        const timer = setInterval(callRef.current, delay);
+        const temp = () => callRef.current();
+        const timer = setInterval(temp, delay);
         return () => clearInterval(timer);
     }, [delay])
-
 }
 
 export default useInterval;
